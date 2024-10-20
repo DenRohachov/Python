d3515970ab00b243d6641a44d21dda7345acc3b1
DenRohachov <denis.comfy@gmail.com>
2024-10-20 16:10:52 +0300

    update lesson 4

---
+++ HW/HW-03.py
@@ -0,0 +1,85 @@
+# HW 3
+s1 = "hot"
+s2 = "dog"
+print(s1 + s2)
+
+
+str1 = "my isname isisis jameis isis bond"
+sub = "is"
+print(str1.count(sub, 4))
+
+str = "my name is James bond"
+print (str.capitalize())
+
+str1 = "My salary is 7000";
+str2 = "7000"
+
+print(str1.isdigit())
+print(str2.isdigit())
+
+
+print("John" > "Jhon")
+print("Emma" < "Emm")
+
+first_name = "television"
+hobby = "homer"
+tmp = first_name
+first_name = hobby
+hobby = tmp
+print(f"T{first_name[1:3]} likes to watch {hobby[4:]}")
+
+
+str1 = "Welcome"
+print(str1*2)
+
+
+str1 = "My salary is 7000";
+str2 = "7000"
+
+print(str1.isdigit())
+print(str2.isdigit())
+
+str = "my name is James bond"
+print (str.capitalize())
+
+
+print("John" > "Jhon")
+print("Emma" < "Emm")
+
+str1 = "my isname isisis jameis isis bond"
+sub = "is"
+print(str1.count(sub, 4))
+
+
+first_name = "television"
+hobby = "homer"
+tmp = first_name
+first_name = hobby
+hobby = tmp
+print(f"T{first_name[1:3]} likes to watch {hobby[4:]}")
+
+
+str1 = "Ault\"Kelly"
+print(str1)
+
+
+print(bool(0), bool(3.14159), bool(-3), bool(1.0+1j))
+
+v1 = 1
+v2 = 2
+v3 = "3"
+# print(v1 + v2 + v3)
+
+value = 36 / 4 * (3 + 2) * 4 + 2
+print(value)
+
+print(type({}) is set)
+
+value_one = 5 ** 2
+value_two = 5 ** 3
+
+print(value_one)
+print(value_two)
+
+
+print("hello")
