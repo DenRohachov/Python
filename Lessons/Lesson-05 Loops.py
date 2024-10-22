fcd9b3d9b3db5ea7c3b3ae42a8ba7deb7f1eaab6
DenRohachov <denis.comfy@gmail.com>
2024-10-22 21:25:03 +0300

    L05

--- Lessons/Lesson-05 Loops.py
+++ Lessons/Lesson-05 Loops.py
@@ -73,3 +73,100 @@ print(d.items())
 print(a,b)
 for key, value in d.items():
     print(key,value)
+
+
+n = 5
+l = [i for i in range(n)]
+t = (i for i in range(n))
+print(l)
+print(list(t))
+print(t)
+
+
+
+print()
+print("51 хв")
+
+l = [1,2,3]
+for i in l:
+    print(i)
+
+for i in range(3,9,2):
+    print([i])
+
+
+print()
+print("break")
+print("i\t i**2 \t i**i")
+for i in range(5):
+    print(i, end=" \t ")
+    print(i**2, end=" \t ")
+    print(i**i)
+
+
+print()
+print("continue")
+
+for i in range (10):
+    print(i, end="")
+    if i% 2:
+        print()
+        continue
+    print("=>", i**i)
+
+
+print()
+print("break")
+
+for i in range (10):
+    print(i, end="")
+    if i == 3:
+        print()
+        break
+    print("=>", i**i)
+
+
+print()
+print("else з continue")
+
+l = [1,5,9, [1,8,9]]
+s = 0
+for i in l:
+    if type(i) is not int:
+        temp =i
+        continue
+    s += i
+else:
+    print(f"{s=}")
+print("end")
+
+
+print()
+print("pass")
+pass
+
+
+print()
+print("tasks")
+
+for i in range(0,100,2):
+    print(i)
+
+
+i = 0
+while i < 100:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+
+for i in range(1,100,2):
+    print(i)
+
+
+i = 1
+while i < 100:
+    i += 1
+    if i % 2 ==0:
+        continue
+    print(i)
+
